b4cff2f2be28319e16d88f709256ce20886239a3
Samuel Ketema <samuelketema01@gmail.com>
2025-12-06 21:40:35 +0300

    schema for db

---
+++ frontend/taskapp/backend/src/db/schema.ts
@@ -0,0 +1,16 @@
+import { timestamp } from "drizzle-orm/pg-core";
+import { text } from "drizzle-orm/pg-core";
+import { pgTable, uuid} from "drizzle-orm/pg-core";
+
+export const users = pgTable("users",{
+    id:uuid("id").primaryKey().defaultRandom(),
+    name:text("name").notNull(),
+    email:text("email").notNull().unique(),
+    password:text("password").notNull(),
+    createdAt:timestamp("created_at").defaultNow(),
+    updatedAt:timestamp("updated_at").defaultNow(),
+
+});
+
+export type User = typeof users.$inferSelect
+export type NewUser = typeof users.$inferInsert
